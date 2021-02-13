8c8013e8d1dec3a256c73aefbd65ffc6593bd381
Wojciech Kaczmarzyński <wojciech.kaczmarzynski@gmail.com>
2021-02-13 12:17:15 +0100

    Fix OrderForm

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-//import styles from './OrderForm.scss';
 import {Row, Col} from 'react-flexbox-grid';
 import OrderSummary from '../OrderSummary/OrderSummary';
 
 const OrderForm = ({tripCost, options}) => (
   <Row>
     <Col xs={12}>
-      <OrderSummary tripCost={tripCost} options={options}>
-
-      </OrderSummary>
+      <OrderSummary tripCost={tripCost} options={options} />
     </Col>
   </Row>
 );
