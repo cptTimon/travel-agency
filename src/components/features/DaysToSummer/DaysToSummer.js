8468819e7666d6764446594af976814dd499d5fa
Wojciech Kaczmarzyński <wojciech.kaczmarzynski@gmail.com>
2021-03-04 10:58:05 +0100

    Task DaysToSummer

--- src/components/features/DaysToSummer/DaysToSummer.js
+++ src/components/features/DaysToSummer/DaysToSummer.js
@@ -3,20 +3,20 @@ import PropTypes from 'prop-types';
 import { calculateDaysToSummer } from '../../../utils/calculateDaysToSummer';
 import styles from './DaysToSummer.scss';
 
-class DaysToSummer extends React.Component {
-  render() {
-    const {date=new Date()} = this.props;
-    return (
-      <div>
-        <h3 className={styles.title}>
-          {calculateDaysToSummer(date) !== null ? calculateDaysToSummer(date) : '' }
-        </h3>
-      </div>
-    );
-  }
-}
+const DaysToSummer = ({date}) => (
+  <div>
+    <h3 className={styles.title}>
+      {calculateDaysToSummer(date) !== null ? calculateDaysToSummer(date) : '' }
+    </h3>
+  </div>
+);
+
 DaysToSummer.propTypes = {
   date: PropTypes.instanceOf(Date),
+};
+
+DaysToSummer.defaultProps = {
+  date: new Date(),
 };
 
 export default DaysToSummer;
